e33073abe9be3a0f69cbef140a1322545095f18d
katoshin <hatao0000000@outlook.com>
2022-02-14 23:40:37 +0900

    browser detect を用いてブラウザを特定

--- src/manager/SoundManager.ts
+++ src/manager/SoundManager.ts
@@ -1,3 +1,5 @@
+import { BrowserInfo, BotInfo, NodeInfo, detect, SearchBotDeviceInfo, ReactNativeInfo } from "detect-browser";
+
 export default class SoundManager {
   public static instance: SoundManager;
 
@@ -11,5 +13,31 @@ export default class SoundManager {
     if (SoundManager.instance) {
       throw new Error('SoundManager can not be initialized twice');
     }
+  }
+
+  public static init(ctx?: AudioContext): void {
+    if (SoundManager.instance) {
+      return;
+    }
+    
+    SoundManager.instance = new SoundManager();
+
+    if (ctx) {
+      SoundManager.context = ctx;
+    } else {
+      const AudioContextClass = (window as any).AudioContext || (window as any).webkitAudioContext;
+      SoundManager.context = new AudioContextClass();
+    }
+
+    const browser = detect();
+    if (!browser) {
+      return;
+    }
+
+    SoundManager.setSoundInitializeEvent(browser);
+  }
+
+  public static setSoundInitializeEvent(browser: BrowserInfo | BotInfo | NodeInfo | SearchBotDeviceInfo | ReactNativeInfo): void {
+    console.log(browser)
   }
 }
